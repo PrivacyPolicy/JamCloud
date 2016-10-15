5368bd3d59ed096bf516e1d699d8fabd546a9a1b
root <root@packer-debian-8-amd64.digitalocean.com>
2016-10-15 22:32:54 +0000

    Created Full clipcommand interface, but not tested

--- Backend/clipcommand.php
+++ Backend/clipcommand.php
@@ -6,14 +6,15 @@
 	$g_ip = $_SERVER['REMOTE_ADDR'];
 	$action = $_POST['ACTION'];
 	$g_id = $_POST['OBJECT'];
-	$g_data = $_POST['DATA'];
+	$g_data = mysqli_real_escape_string($link, $_POST['DATA']);
 
 	/* create an object and put in data if necessary */
 	function createObject($id, $data){
 		global $link;
 		$result = mysqli_query($link, "SELECT * FROM Objects WHERE ID=$id;");
 		if(mysqli_fetch_array($result) == false){
-			$query = "INSERT INTO Objects (ID, DATA) VALUES($id, $data);";
+			$query = "INSERT INTO Objects (ID, DATA) VALUES($id, '$data');";
+			echo($query);
 			$dp = mysqli_query($link, $query);
 		
 			var_dump($dp);
@@ -25,10 +26,40 @@
 	/* Delete object completely*/
 	function deleteObject($id){
 		global $link;
-		$result = mysqli_query($link, "SELECT * FROM Objects WHERE ID=$g_id and IP=NULL");
+		$result = mysqli_query($link, "SELECT * FROM Objects WHERE ID=$id and IP=NULL");
 		if(mysqli_fetch_array($result) !== false){
-			mysqli_query($link, "DELETE FROM Objects WHERE ID=$g_id");
+			mysqli_query($link, "DELETE FROM Objects WHERE ID=$id");
 			return true;
+		}else{
+			return false;
+		}
+	}
+	function requestEditObject($id, $ip){
+		global $link;
+		$result = mysqli_query($link, "SELECT * FROM Objects WHERE ID=$ip and IP=NULL");
+		if(mysqli_fetch_array($result) !== false){
+			mysqli_query($link, "UPDATE Objects SET IP=$ip WHERE ID=$id");
+			return true;
+		}else{
+			return false;
+		}
+	}
+	function quitEditObject($id, $ip){
+		global $link;
+		$result = mysqli_query($link, "SELECT * FROM Objects WHERE ID=$id and IP=$ip");
+		if(mysqli_fetch_array($result) !== false){
+			mysqli_query($link, "UPDATE Objects SET IP=NULL WHERE ID=$id");
+			return true;
+		}else{
+			return false;
+		}
+	}
+		
+	function updateObject($id, $ip, $data){
+		global $link;
+		$result = mysqli_query($link, "SELECT * form oBJECTS where ID=$id and IP=$ip");
+		if(mysqli_fetch_array($result)!==false){
+			mysqli_query($link, "UPDATE OBJECTS SET DATA=$data WHERE ID=$id"); return true;
 		}else{
 			return false;
 		}
@@ -38,8 +69,13 @@
 		$rval = createObject($g_id, $g_data);
 	}else if($action=="DELETE"){
 		$rval = deleteObject($g_id);
+	}else if($action=="RQEDIT"){
+		$rval = requestEditObject($g_id,$g_ip);
+	}else if($action=="QTEDIT"){
+		$rval = quitEditObject($g_id,$g_ip);
+	}else if($action=="UPDATE"){
+		$rval = updateObject($g_id,$g_ip,$data);
 	}
-
 	echo($rval);
 
 ?>
