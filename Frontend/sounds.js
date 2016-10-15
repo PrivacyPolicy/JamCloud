0f152f89c4bc2966129fbc665e6e254218a8f9c3
Gabriel Hutchison <ghutchison2600@flpoly.org>
2016-10-15 19:00:09 -0400

    Fixed sounds.js errors

--- Frontend/sounds.js
+++ Frontend/sounds.js
@@ -11,19 +11,12 @@ function Sound(src){
 }
 
 //Discrete notes from a certain instrument
-function Note(instrument, tone){
+function Note(instrument, tone, callback){
 	//Load an instrument from the online repo
 	//Do this by entering your own URL or just using the default https://gleitz.github.io/midi-js-soundfonts/
-	this.instr = Soundfont.instrument(ac, instrument); 
-	//e.g. 'C4', 'G3', etc.
-	this.t = tone;
-
-	//guess what this does......
-	this.play = function(){
-		this.instr.play(this.t);
-	}
-
-	function stop(){
-		instr.stop();
-	}
+	Soundfont.instrument(ac, instrument)
+        .then(function(instrument) {
+        
+        instrument.play(tone);
+    }); 
 }
