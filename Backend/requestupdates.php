0a37c3915a11128b79af727c42fc3e2f2efe1513
Gabriel Hutchison <ghutchison2600@flpoly.org>
2016-10-16 03:31:17 -0400

    Finished implementing requestUpdates functionality

--- Backend/requestupdates.php
+++ Backend/requestupdates.php
@@ -2,3 +2,22 @@
 /* The clients submits a GET request to receive a list of all updates
    since the GET-given timestamp
    */
+include("./session.php");
+$g_timestamp = $_GET["TIMESTAMP"];
+$result = mysqli_query($link, "SELECT * FROM Updates WHERE TIMESTAMP >= $g_timestamp ORDER BY TIMESTAMP;");
+$rows;
+if (($rows = mysqli_fetch_array($result)) != false) {
+    echo "[\n";
+    foreach ($i = 0; $i < len($rows); $i++) {
+        echo "{\"timestamp\": {$row["TIMESTAMP"]}, ";
+        echo "\"class\": {$row["CLASS"]}, ";
+        echo "\"objectID\": {$row["OBJ_ID"]}, ";
+        echo "\"action\": {$row["ACTION"]}, ";
+        echo "\"data\": {$row["DATA"]}";
+        echo "}";
+        if ($i < len($rows)) {
+            echo ",\n";
+        }
+    }
+    echo "\n]\n";
+}
