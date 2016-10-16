16c5de9b473682cf34ecab4e9ab749c1e93c60e9
Gabriel Hutchison <ghutchison2600@flpoly.org>
2016-10-16 02:32:46 -0400

    fixed issue where play button failed to work

--- Frontend/interface.js
+++ Frontend/interface.js
@@ -73,13 +73,10 @@ function buildTable() {
 function playAll(){
 	//Concatenate like instrument sounds together
 	var allnotes=[];
-	for (var i=0; i<data.instruments.length; i++){
-	
-		for (var j=0; j<data.instruments[i].clips.length; j++){
-			allnotes = allnotes.concat(data.instruments[i].clips[j].notes);
-		}
-	}
-	//Play each instrument separately them all as a single instrument call	
+    for (var j=0; j < data.clips.length; j++){
+        allnotes = allnotes.concat(data.clips[j].notes);
+    }
+	// Play each instrument separately them all as a single instrument call	
 	playNoteSeries("acoustic_grand_piano" , allnotes);
 	stepTimerBar();
 }
